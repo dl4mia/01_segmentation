02d2c9e0797cde8a9a675f29cd559f50cd99371e
Larissa Heinrich <heinrichl@janelia.hhmi.org>
2024-03-26 18:36:12 -0400

    fix: :bug: fix data download by switching to using gdown python tool

--- setup.sh
+++ setup.sh
@@ -8,9 +8,10 @@ mamba install -c pytorch -c nvidia --file requirements.txt -y
 # Build the notebooks
 sh prepare-exercise.sh
 
+# Download and extract data, etc.
+#wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1L344AoTTx-mu9MyNt-2iZ5A3ww3tC_Zp' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1L344AoTTx-mu9MyNt-2iZ5A3ww3tC_Zp" -O kaggle_data.zip && rm -rf /tmp/cookies.txt
+gdown -O kaggl_data.zip 1L344AoTTx-mu9MyNt-2iZ5A3ww3tC_Zp
+unzip -u -qq kaggle_data.zip && rm kaggle_data.zip
+
 # Return to base environment
 mamba activate base
-
-# Download and extract data, etc.
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1L344AoTTx-mu9MyNt-2iZ5A3ww3tC_Zp' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1L344AoTTx-mu9MyNt-2iZ5A3ww3tC_Zp" -O kaggle_data.zip && rm -rf /tmp/cookies.txt
-unzip -u -qq kaggle_data.zip && rm kaggle_data.zip
