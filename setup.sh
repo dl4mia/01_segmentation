878acc7ccc4a42a65ae9e5d8663ceefd8680b1e3
lmanan <manan.lalit@gmail.com>
2024-04-14 17:09:34 -0400

    Add link to zip file containing instance masks for same dataset.

--- setup.sh
+++ setup.sh
@@ -13,9 +13,11 @@ sh prepare-exercise.sh
 gdown -O kaggle_data.zip 1L344AoTTx-mu9MyNt-2iZ5A3ww3tC_Zp
 unzip -u -qq kaggle_data.zip && rm kaggle_data.zip
 
+# Download and extract data for the instance segmentation exercise
+gdown -O kaggle_data_instance.zip 1I-TLOwwwNVdNc-AKjUw799j2A6cP-qaZ 
+unzip -u -qq kaggle_data_instance.zip && rm kaggle_data_instance.zip
+
+
 # Return to base environment
 mamba deactivate
 
-# Download and extract data, etc.
-# wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1L344AoTTx-mu9MyNt-2iZ5A3ww3tC_Zp' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1L344AoTTx-mu9MyNt-2iZ5A3ww3tC_Zp" -O kaggle_data.zip && rm -rf /tmp/cookies.txt
-# unzip -u -qq kaggle_data.zip && rm kaggle_data.zip
