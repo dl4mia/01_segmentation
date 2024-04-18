9b20dd4ce670114ecf09b46985e4e6140be385dd
lmanan <manan.lalit@gmail.com>
2024-04-18 09:46:45 -0400

    Update url to drive.
    Now, both `label.tif` and `mask.tif` are obtained from the same url.

--- setup.sh
+++ setup.sh
@@ -10,12 +10,8 @@ sh prepare-exercise.sh
 
 # Download and extract data, etc.
 #wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1L344AoTTx-mu9MyNt-2iZ5A3ww3tC_Zp' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1L344AoTTx-mu9MyNt-2iZ5A3ww3tC_Zp" -O kaggle_data.zip && rm -rf /tmp/cookies.txt
-gdown -O kaggle_data.zip 1L344AoTTx-mu9MyNt-2iZ5A3ww3tC_Zp
+gdown -O kaggle_data.zip 1wEsq8nf9Sr9z17aYfVzXNxzu6SD9JbXa
 unzip -u -qq kaggle_data.zip && rm kaggle_data.zip
-
-# Download and extract data for the instance segmentation exercise
-gdown -O kaggle_data_instance.zip 1I-TLOwwwNVdNc-AKjUw799j2A6cP-qaZ 
-unzip -u -qq kaggle_data_instance.zip && rm kaggle_data_instance.zip
 
 
 # Return to base environment
