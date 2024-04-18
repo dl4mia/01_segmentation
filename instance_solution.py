70b8de4a61ee14a8a7cb7f722983d144e97e428a
ahillsley <avhillsley@utexas.edu>
2024-04-18 11:15:12 -0400

    improve instance

--- instance_solution.py
+++ instance_solution.py
@@ -3,12 +3,23 @@
 #
 # <hr style="height:2px;">
 #
-# In this notebook, we adapt our 2D U-Net for better nuclei segmentations in the Kaggle Nuclei dataset.
+# - So far we were only interested in semantic classes, e.g. foreground / background, cell types, person / car, etc. 
+# But in many cases we not only want to know if a certain pixel belongs to an object, but also to which unique object.
 #
+# - For isolated objects, this is trivial, all connected foreground pixels form one instance, yet often instances 
+# are very close together or even overlapping. Thus we need to think a bit more how to formulate the inputs / loss 
+# of our network
+#
+# - Further, in instance segmentation the specific value of each label is arbitrary
+#
+# Manan's Image
+#
+# - Therefore we must devise an auxilliary task to train the model on, which we then can post-process into our final 
+# instance segmentation
 
 # %% [markdown]
 # <div class="alert alert-danger">
-# Please use kernel <code>05-semantic-segmentation</code> for this exercise.
+# Use the segmentation environment
 # </div>
 
 # %% [markdown]
@@ -39,12 +50,6 @@ from tqdm import tqdm
 # %%
 # make sure gpu is available. Please call a TA if this cell fails
 assert torch.cuda.is_available()
-# %% [markdown]
-# ## Visualize the data:
-# For instance segmentation, the specific value of the label is arbitrary
-# This means we cannot train directly on those labels
-# image table of different labels --> say all are equivalently good
-
 
 # %% [markdown]
 # <hr style="height:2px;">
@@ -53,27 +58,54 @@ assert torch.cuda.is_available()
 # what is the signed distance transform, with image examples
 
 # %% [markdown]
-# Create the  DataSet
+# <div class="alert alert-block alert-info">
+# <b>Task 1.1</b>: Write a function to compute the signed distance transform
+# </div>
+# %%
+from scipy.ndimage import distance_transform_edt, binary_erosion
+
+def compute_sdt(labels: np.ndarray, constant: float = ..., scale: int = ...):
+    """Function to compute a signed distance transform."""
+
+    # compute the distance transform inside and outside of the objects
+
+    # create the signed distance transform 
+
+    # scale the distances so that they are between -1 and 1 (hint: np.tanh)
+
+    # be sure to return your solution as type 'float'
+    return 
+
 
 # %% tags=["solution"]
 # write a function to calculate SDT
+# maybe change to a forloop that iterates over all labels, would work for both instance and semantic labels
 from scipy.ndimage import distance_transform_edt, binary_erosion
 
 def compute_sdt(labels: np.ndarray, constant: float = 0.5, scale: int = 5):
     """Function to compute a signed distance transform."""
 
+    # compute the distance transform inside and outside of the objects
     inner = distance_transform_edt(binary_erosion(labels))
     outer = distance_transform_edt(np.logical_not(labels))
 
+    # create the signed distance transform 
     distance = (inner - outer) + constant
 
+    # scale the distances so that they are between -1 and 1 (hint: np.tanh)
     distance = np.tanh(distance / scale)
 
+    # be sure to return your solution as type 'float'
     return distance.astype(float)
 
-#%%
-# small box to visualize the signed distance transform
-from local import NucleiDataset, show_random_dataset_image
+# %% [markdown]
+# Below is a small function to visualize the signed distance transform (SDT), use it to validate your function.
+#
+# Make sure that your SDT output is what you expect
+# %%
+# visualize the signed distance transform
+from local import NucleiDataset
+# import an example dataset
 train_data = NucleiDataset("nuclei_train_data", transforms.RandomCrop(256))
 
 idx = np.random.randint(0, len(train_data))  # take a random sample
@@ -88,15 +120,81 @@ plt.show()
 
 #%% tags [markdown]
 # Questions
-# 1. What is the purpose of the tanh function in computing our signed distance transform?
-# 2. What is the effect of changing the scale value? what do you think is a good default value? why?
+# 1. Why do we need to normalize the distances between -1 and 1?
+#   -
+# 2. What is the effect of changing the scale value? What do you think is a good default value?
+#   -
 
+#%% [markdown] tags=["solution"]
+# Questions
+# 1. Why do we need to normalize the distances between -1 and 1?
+#   -
+# 2. What is the effect of changing the scale value? What do you think is a good default value?
+#   -
 
-#%% tags [solution]
-# take the dataset from local.py and add create SDT target function
-# 1. add a create_sdt_target function to this class
-# 2. Change the __get_item__ method to return the sdt output rather than the mask
-#   2a. think about how transformations will affect the SDT vs mask
+# %% [markdown]
+# <div class="alert alert-block alert-info">
+# <b>Task 1.2</b>: Modify the dataset to produce the paired raw and SDT images
+#   1. use the compute_sdt function to fill in the create_sdt_target method
+#   2. modify the __get_item__ method to return an SDT output rather than a mask
+#       - be sure that all final outputs in a torch tensor
+#       - Think about the order transformations are applied to the mask/SDT
+# </div>
+
+#%% 
+class InstanceDataset(Dataset):
+    """A PyTorch dataset to load cell images and nuclei masks"""
+
+    def __init__(self, root_dir, transform=None, img_transform=None):
+        self.root_dir = root_dir  # the directory with all the training samples
+        self.samples = os.listdir(root_dir)  # list the samples
+        self.transform = (
+            transform  # transformations to apply to both inputs and targets
+        )
+        self.img_transform = img_transform  # transformations to apply to raw image only
+        #  transformations to apply just to inputs
+        self.inp_transforms = transforms.Compose(
+            [
+                transforms.Grayscale(),  # some of the images are RGB
+                transforms.ToTensor(),
+                transforms.Normalize([0.5], [0.5]), # 0.5 = mean and 0.5 = variance 
+            ]
+        )
+
+    # get the total number of samples
+    def __len__(self):
+        return len(self.samples)
+
+    # fetch the training sample given its index
+    def __getitem__(self, idx):
+
+        # Modify this function to return an image and sdt pair
+
+        img_path = os.path.join(self.root_dir, self.samples[idx], "image.tif")
+        # we'll be using Pillow library for reading files
+        # since many torchvision transforms operate on PIL images
+        image = Image.open(img_path)
+        image = self.inp_transforms(image)
+        mask_path = os.path.join(self.root_dir, self.samples[idx], "mask.tif")
+        mask = Image.open(mask_path)
+        if self.transform is not None:
+            # Note: using seeds to ensure the same random transform is applied to
+            # the image and mask
+            seed = torch.seed()
+            torch.manual_seed(seed)
+            image = self.transform(image)
+            torch.manual_seed(seed)
+            mask = self.transform(mask)
+        if self.img_transform is not None:
+            image = self.img_transform(image)
+        return image, sdt
+    
+    def create_sdt_target(...):
+
+       # fill in function
+
+        return 
+#%% tags=["solution"]
 class InstanceDataset(Dataset):
     """A PyTorch dataset to load cell images and nuclei masks"""
 
@@ -147,13 +245,13 @@ class InstanceDataset(Dataset):
         sdt_target = transforms.ToTensor()(sdt_target_array)
         return sdt_target.float()
 
-#%%
-# Adjust the Unet to this new prediction
-# Which loss function should we use
-# train the model
-from local import train, show_random_dataset_image
-
-from unet import UNet
+#%% [markdown]
+# ### Test your function
+# - Create training and validation datasets and data loaders
+# - use `show_random_dataset_image` to verify that your dataset solution is correct
+#   - output should show 2 images: the raw image and the SDT matching the one shown in task 1.1
+# %% 
+from local import show_random_dataset_image
 
 train_data = InstanceDataset("nuclei_train_data", transforms.RandomCrop(256))
 train_loader = DataLoader(train_data, batch_size=5, shuffle=True)
@@ -162,8 +260,32 @@ val_loader = DataLoader(val_data, batch_size=5)
 
 show_random_dataset_image(train_data)
 
-#%%
-# add the augmentations that you want
+# %% [markdown]
+# <div class="alert alert-block alert-info">
+# <b>Task 1.3</b>: Train the Unet
+# </div>
+#%% [markdown]
+#   - loss function hint: [torch losses](https://pytorch.org/docs/stable/nn.html#loss-functions)
+#   - optimizer hint: [torch optimizers](https://pytorch.org/docs/stable/optim.html)
+
+#%% 
+
+from local import train
+import torch
+from unet import UNet
+
+# initialize the model
+
+# choose a loss function
+
+# choose an optimizer
+
+# write a training loop and train for 10 epochs
+
+#%% tags=["solution"]
+from local import train
+from unet import UNet
+
 unet = UNet(
     in_channels=1,
     num_fmaps=64,
@@ -189,25 +311,26 @@ for epoch in range(5):
         device="cpu", # make sure to set to cuda
     )
 
-#%% 
-# take a look at the results
-# evaluate model and plot resulting distance transform
+#%% [markdown]
+# Here we will run inference using our trained model
 
-# This looks good, but it isn't quite what we want yet
-
-#%% 
-# set model to evaluate and visualize the output
-
-idx = np.random.randint(0, len(val_data))  # take a random sample
-img, mask = val_data[idx]  # get the image and the nuclei masks
-
-# make prediction off of mask
+# %%
 unet.eval()
-prediction = unet(torch.unsqueeze(img, dim=0))
+idx = np.random.randint(0, len(val_data))  # take a random sample
+image, mask = val_data[idx]  # get the image and the nuclei masks
+
+image = image.to(device)
+pred = unet(image)
+    
+image = np.squeeze(image.cpu())
+mask = np.squeeze(mask.cpu().numpy())
+    
+pred = np.squeeze(pred.cpu().detach().numpy())
+
 f, axarr = plt.subplots(1, 3)  # make two plots on one figure
-axarr[0].imshow(img[0])  # show the image
-axarr[1].imshow((mask[0]), interpolation=None)  # show the masks
-axarr[2].imshow(prediction.cpu().detach().numpy()[0][0]) #
+axarr[0].imshow(image)  # show the image
+axarr[1].imshow((mask), interpolation=None)  # show the masks
+axarr[2].imshow(pred) # show the prediction
 _ = [ax.axis("off") for ax in axarr]  # remove the axes
 plt.show()
 
@@ -218,13 +341,42 @@ plt.show()
 # <hr style="height:2px;">
 #
 # ## Section 2: Post-Processing
-# What is watershed?
-# How do we get seed points?
+# - See here for a nice overview: [scikit-image watershed](https://scikit-image.org/docs/stable/auto_examples/segmentation/plot_watershed.html)
+# 1. Given the distance transform (the output of our model), we first need to find the local maxima that will be used as seed points
+# 2. The watershed algorithm then expands each seed out in a local "basin" until the segments touch.
 
-#%%
+# %% [markdown]
+# <div class="alert alert-block alert-info">
+# <b>Task 2.1</b>: write a function to find the local maxima of the distance transform
+# </div>
+
+# %% [markdown]
+# hint: look at the imports
+# hint: it is possible to write this function by only adding 2 lines
+# %% 
 from scipy.ndimage import label, maximum_filter
-from skimage.segmentation import watershed
 
+def find_local_maxima(distance_transform, min_seed_distance):
+
+    # fill in function here...
+    
+    # uniquely label the local maxima 
+    seeds, n = label(...)
+
+    return seeds, n
+#%% tags=["solution"]
+from scipy.ndimage import label, maximum_filter
+def find_local_maxima(distance_transform, min_seed_distance):
+    max_filtered = maximum_filter(distance_transform, min_seed_distance)
+    maxima = max_filtered==distance_transform
+    seeds, n = label(maxima)
+
+    return seeds, n
+
+# %% [markdown]
+# We now use this function to find the seeds for the watershed
+# %% 
+from skimage.segmentation import watershed
 
 def watershed_from_boundary_distance(
         boundary_distances: np.ndarray,
@@ -234,11 +386,8 @@ def watershed_from_boundary_distance(
         ):
     """Function to compute a watershed from boundary distances."""
 
-    # get our seeds 
-    # make them write a function to find maximum values?
-    max_filtered = maximum_filter(boundary_distances, min_seed_distance)
-    maxima = max_filtered==boundary_distances
-    seeds, n = label(maxima)
+    
+    seeds, n = find_local_maxima(boundary_distances, min_seed_distance)
 
     if n == 0:
         return np.zeros(boundary_distances.shape, dtype=np.uint64), id_offset
@@ -257,27 +406,56 @@ def get_boundary_mask(pred, threshold):
     boundary_mask = pred > threshold
     return boundary_mask
 
-#%%
-#unet.eval()
+# %% [markdown]
+# <div class="alert alert-block alert-info">
+# <b>Task 2.2</b>: get the watershed segmentation
+# </div>
+
+# %% 
+idx = np.random.randint(0, len(val_data))  # take a random sample
+image, mask = val_data[idx]  # get the image and the nuclei masks
+
+# hint: look at how we got predictions in the inference box above
+# hint: make sure set the model to evaluation
+
+
+# chose a threshold value to use to get the boundary mask
+thresh = ...
+
+# get boundary mask
+boundary_mask = ...
+
+# get the segmentation
+seg = watershed_from_boundary_distance(...)
+
+# plot the results
+f, axarr = plt.subplots(1, 3)  # make two plots on one figure
+axarr[0].imshow(image)  # show the image
+axarr[1].imshow((mask), interpolation=None)  # show the masks
+axarr[2].imshow((seg))
+_ = [ax.axis("off") for ax in axarr]  # remove the axes
+plt.show()
+
+# %% tags=["solution"]
+# If we notice this exercise is running long, we can just give them this part
+
+unet.eval()
 idx = np.random.randint(0, len(val_data))  # take a random sample
 image, mask = val_data[idx]  # get the image and the nuclei masks
 
 image = image.to(device)
-#pred = unet(image)
+pred = unet(image)
     
 image = np.squeeze(image.cpu())
 mask = np.squeeze(mask.cpu().numpy())
     
-#pred = np.squeeze(pred.cpu().detach().numpy())
-pred = mask
+pred = np.squeeze(pred.cpu().detach().numpy())
 
 # feel free to try different thresholds
 thresh = np.mean(pred)
 
-
 # get boundary mask
 boundary_mask = get_boundary_mask(pred, threshold=thresh)
-
 
 seg = watershed_from_boundary_distance(
     pred,
@@ -293,8 +471,13 @@ _ = [ax.axis("off") for ax in axarr]  # remove the axes
 plt.show()
 
 #%% [markdown]
+# Questions:
 # 1. what is the effect of the min_seed_distance parameter in watershed? 
-# try really high and really low values
+#   - experiment with different values
+
+# %% [markdown]
+# <div class="alert alert-block alert-success">
+# <h2> Checkpoint 1 </h2>
 
 # %% [markdown]
 # <hr style="height:2px;">
