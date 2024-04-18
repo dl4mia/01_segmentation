73ad56d6aaa0fe8272dbb3cd989c04fb5371e5c6
lmanan <manan.lalit@gmail.com>
2024-04-18 15:36:22 -0400

    Update download link.

--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@ sh prepare-exercise.sh
 
 # Download and extract data, etc.
 #wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1L344AoTTx-mu9MyNt-2iZ5A3ww3tC_Zp' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1L344AoTTx-mu9MyNt-2iZ5A3ww3tC_Zp" -O kaggle_data.zip && rm -rf /tmp/cookies.txt
-gdown -O kaggle_data.zip 1wEsq8nf9Sr9z17aYfVzXNxzu6SD9JbXa
+gdown -O kaggle_data.zip 1ahuduC_4Ex84R7qKNRzAY6PiLRWX_J3I
 unzip -u -qq kaggle_data.zip && rm kaggle_data.zip
 
 
