a89e6827947fa8c9cf8cb2f652af697e8242c069
ahillsley <avhillsley@utexas.edu>
2024-05-08 10:54:13 +0200

    re-add wills fix to the dataloader

--- 3-instance_solution.py
+++ 3-instance_solution.py
@@ -174,12 +174,27 @@ class SDTDataset(Dataset):
         #  transformations to apply just to inputs
         inp_transforms = transforms.Compose(
             [
-                transforms.Greyscale(),
+                transforms.Grayscale(),
                 transforms.ToTensor(),
                 transforms.Normalize([0.5], [0.5]),  # 0.5 = mean and 0.5 = variance
             ]
         )
 
+        self.loaded_imgs = [None] * len(self.samples)
+        self.loaded_masks = [None] * len(self.samples)
+        for sample_ind in range(len(self.samples)):
+            img_path = os.path.join(
+                self.root_dir, self.samples[sample_ind], "image.tif"
+            )
+            image = Image.open(img_path)
+            image.load()
+            self.loaded_imgs[sample_ind] = inp_transforms(image)
+            mask_path = os.path.join(
+                self.root_dir, self.samples[sample_ind], "label.tif"
+            )
+            mask = Image.open(mask_path)
+            mask.load()
+            self.loaded_masks[sample_ind] = mask
         
     # get the total number of samples
     def __len__(self):
@@ -190,13 +205,10 @@ class SDTDataset(Dataset):
 
         #  TODO: Modify this function to return an image and sdt pair
 
-        img_path = os.path.join(self.root_dir, self.samples[idx], "image.tif")
         # we'll be using the Pillow library for reading files
         # since many torchvision transforms operate on PIL images
-        image = Image.open(img_path)
-        image = self.inp_transforms(image)
-        mask_path = os.path.join(self.root_dir, self.samples[idx], "label.tif")
-        mask = Image.open(mask_path)
+        image = self.loaded_imgs[idx]
+        mask = self.loaded_masks[idx]
         if self.transform is not None:
             # Note: using seeds to ensure the same random transform is applied to
             # the image and mask
@@ -234,7 +246,7 @@ class SDTDataset(Dataset):
         )
         self.img_transform = img_transform  # transformations to apply to raw image only
         #  transformations to apply just to inputs
-        self.inp_transforms = transforms.Compose(
+        inp_transforms = transforms.Compose(
             [
                 transforms.Grayscale(),
                 transforms.ToTensor(),
@@ -242,7 +254,21 @@ class SDTDataset(Dataset):
             ]
         )
 
-        
+        self.loaded_imgs = [None] * len(self.samples)
+        self.loaded_masks = [None] * len(self.samples)
+        for sample_ind in range(len(self.samples)):
+            img_path = os.path.join(
+                self.root_dir, self.samples[sample_ind], "image.tif"
+            )
+            image = Image.open(img_path)
+            image.load()
+            self.loaded_imgs[sample_ind] = inp_transforms(image)
+            mask_path = os.path.join(
+                self.root_dir, self.samples[sample_ind], "label.tif"
+            )
+            mask = Image.open(mask_path)
+            mask.load()
+            self.loaded_masks[sample_ind] = mask
 
     # get the total number of samples
     def __len__(self):
@@ -250,13 +276,10 @@ class SDTDataset(Dataset):
 
     # fetch the training sample given its index
     def __getitem__(self, idx):
-        img_path = os.path.join(self.root_dir, self.samples[idx], "image.tif")
         # We'll be using the Pillow library for reading files
         # since many torchvision transforms operate on PIL images
-        image = Image.open(img_path)
-        image = self.inp_transforms(image)
-        mask_path = os.path.join(self.root_dir, self.samples[idx], "label.tif")
-        mask = Image.open(mask_path)
+        image = self.loaded_imgs[idx]
+        mask = self.loaded_masks[idx]
         if self.transform is not None:
             # Note: using seeds to ensure the same random transform is applied to
             # the image and mask
@@ -339,9 +362,10 @@ for epoch in range(20):
         log_interval=10,
         device=device,
     )
+
 # %% tags=["solution"]
 unet = UNet(
-    depth=1,
+    depth=2,
     in_channels=1,
     out_channels=1,
     final_activation="Tanh",
@@ -634,7 +658,7 @@ class AffinityDataset(Dataset):
         )
         self.img_transform = img_transform  # transformations to apply to raw image only
         #  transformations to apply just to inputs
-        self.inp_transforms = transforms.Compose(
+        inp_transforms = transforms.Compose(
             [
                 transforms.Grayscale(),  # some of the images are RGB
                 transforms.ToTensor(),
@@ -642,19 +666,30 @@ class AffinityDataset(Dataset):
             ]
         )
 
+        self.loaded_imgs = [None] * len(self.samples)
+        self.loaded_masks = [None] * len(self.samples)
+        for sample_ind in range(len(self.samples)):
+            img_path = os.path.join(
+                self.root_dir, self.samples[sample_ind], "image.tif"
+            )
+            image = Image.open(img_path)
+            image.load()
+            self.loaded_imgs[sample_ind] = inp_transforms(image)
+            mask_path = os.path.join(
+                self.root_dir, self.samples[sample_ind], "label.tif"
+            )
+            mask = Image.open(mask_path)
+            mask.load()
+            self.loaded_masks[sample_ind] = transforms.ToTensor()(mask)
+
     # get the total number of samples
     def __len__(self):
         return len(self.samples)
 
     # fetch the training sample given its index
     def __getitem__(self, idx):
-        img_path = os.path.join(self.root_dir, self.samples[idx], "image.tif")
-        # we'll be using Pillow library for reading files
-        # since many torchvision transforms operate on PIL images
-        image = Image.open(img_path)
-        image = self.inp_transforms(image)
-        mask_path = os.path.join(self.root_dir, self.samples[idx], "label.tif")
-        mask = Image.open(mask_path)
+        image = self.loaded_imgs[idx]
+        mask = self.loaded_masks[idx]
         if self.transform is not None:
             # Note: using seeds to ensure the same random transform is applied to
             # the image and mask
